86ae3d699ff98a48f7c5e377c32241a57d05560e
Yosnier Samon M <yosamac@gmail.com>
2017-07-18 00:48:36 +0200

    Added notifications

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    notify = require("gulp-notify"),
     browserSync = require('browser-sync').create();
+
 
 gulp.task("default", function(){
     browserSync.init({server:"src/"}); //Starting browsersync on the  src folder
     gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["sass"]); // execute the sass task
     gulp.watch("src/*.html").on("change", browserSync.reload); //reload the html files
+    gulp.watch("src/*.html", function(){
+        browserSync.reload;
+        notify().write("Browser reloaded");
+    } )
+
+
 });
 
 // Compilar sass
 gulp.task("sass", function(){
     gulp.src("src/scss/style.scss") //Loaded the style.scss file
-        .pipe(sass().on("error", sass.logError)) //compile
+        .pipe(sass().on("error", function(error){
+            return notify().write(error) // Show notification if there is an error
+        })) //compile
         .pipe(gulp.dest("src/css/")) //save
-        .pipe(browserSync.stream()); // reload the css on browser
+        .pipe(browserSync.stream())// reload the css on browser
+        .pipe(notify("SASS compilado")); //Show notifactions on screen
 });
