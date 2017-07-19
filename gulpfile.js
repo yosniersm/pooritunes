c6a82e42906325907ccf929e19038b55a13af3f1
Yosnier Samon M <yosamac@gmail.com>
2017-07-19 22:09:34 +0200

    Added gulp-html-import plugin and dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     notify = require("gulp-notify"),
+    gulpImport = require("gulp-html-import"),
     browserSync = require('browser-sync').create();
 
 
-gulp.task("default", function(){
-    browserSync.init({server:"src/"}); //Starting browsersync on the  src folder
+gulp.task("default",["html","sass"], function(){
+    browserSync.init({server:"dist/"}); //Starting browsersync on the  src folder
     gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["sass"]); // execute the sass task
     gulp.watch("src/*.html").on("change", browserSync.reload); //reload the html files
     gulp.watch("src/*.html", function(){
         browserSync.reload;
         notify().write("Browser reloaded");
     } )
+
+    gulp.watch("src/*.html",["html"]);
 
 
 });
@@ -22,7 +25,16 @@ gulp.task("sass", function(){
         .pipe(sass().on("error", function(error){
             return notify().write(error) // Show notification if there is an error
         })) //compile
-        .pipe(gulp.dest("src/css/")) //save
+        .pipe(gulp.dest("dist/")) //save
         .pipe(browserSync.stream())// reload the css on browser
-        .pipe(notify("SASS compilado")); //Show notifactions on screen
+        .pipe(notify("SASS compiled")); //Show notifactions on screen
 });
+
+//copy and import html
+gulp.task("html", function(){
+    gulp.src("src/*.html")
+        .pipe(gulpImport("src/components/"))
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream())
+        .pipe(notify("HTML imported"))
+})
